a971343aef591eaaeb69570429df0eb0823935bc
ConnorBritton <connorbritton2015@gmail.com>
2020-10-08 17:02:22 -0700

    Added grid to App.js

--- src/App.js
+++ src/App.js
@@ -20,10 +20,41 @@ function generateGrid(rows, columns, mapper) {
 const newTicTacToeGrid = () =>
   generateGrid(3, 3, () => null)
 
+function Grid({ grid }) {
+  return (
+    <div style={{ display: 'inline-block' }}>
+      <div
+        style={{
+          backgroundColor: '#000',
+          display: 'grid',
+          gridTemplateRows: `repeat(${grid.length}, 1fr)`,
+          gridTemplateColumns: `repeat(${grid[0].length}, 1fr)`,
+          gridGap: 2,
+        }}
+      >
+        {grid.map((row, rowIdx) =>
+          row.map((cell, colIdx) => (
+            <Cell key={`${colIdx}-${rowIdx}`} cell={cell} />          
+          ))
+        )}
+      </div>
+    </div>
+  )
+}
+
+const cellStyle = {
+  backgroundColor: '#fff',
+  height: 75,
+  width: 75,
+}
+
+function Cell({ cell }) {
+  return <div style={cellStyle}>{cell}</div>
+}
+
 function Game() {
   const grid = newTicTacToeGrid()
-  console.log(grid)
-  return <div>Game</div>
+  return <Grid grid={grid} />
 }
 
 export default App;
