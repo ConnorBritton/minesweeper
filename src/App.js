053397f828c45d92966dfd2e968a10d7ea1dd597
ConnorBritton <connorbritton2015@gmail.com>
2020-10-14 06:25:09 -0600

    updated appjs

--- src/App.js
+++ src/App.js
@@ -13,28 +13,43 @@ function App() {
 }
 
 function generateGrid(rows, columns, mapper) {
-  return Array(rows).fill().map(() =>
-    Array(columns).fill().map(mapper))
+  return Array(rows)
+    .fill()
+    .map(() =>
+      Array(columns)
+        .fill()
+        .map(mapper)
+    )
 }
 
-const newTicTacToeGrid = () =>
-  generateGrid(3, 3, () => null)
+const newTicTacToeGrid = () => generateGrid(3, 3, () => null)
 
 function Grid({ grid }) {
   return (
+    // Wrapping the grid with a div of inline-block means that the grid
+    // takes up only the space defined by the size of the cells, while
+    // still allowing us to use fractional values for the grid-template-*
+    // properties
     <div style={{ display: 'inline-block' }}>
       <div
         style={{
+          // We set a background color to be revealed as the lines
+          // of the board with the `grid-gap` property
           backgroundColor: '#000',
           display: 'grid',
+          // Our rows are equal to the length of our grid
           gridTemplateRows: `repeat(${grid.length}, 1fr)`,
+          // Our columns are equal to the length of a row
           gridTemplateColumns: `repeat(${grid[0].length}, 1fr)`,
           gridGap: 2,
         }}
       >
         {grid.map((row, rowIdx) =>
           row.map((cell, colIdx) => (
-            <Cell key={`${colIdx}-${rowIdx}`} cell={cell} />          
+            // We put the colIdx first because that is our X-axis value
+            // and the rowIdx second because that is our Y-axis value
+            // Getting in the habit makes using 2d grids much easier
+            <Cell key={`${colIdx}-${rowIdx}`} cell={cell} />
           ))
         )}
       </div>
@@ -58,10 +73,10 @@ function Cell({ cell, handleClick }) {
   )
 }
 
-// deeply clone array or object
+// Simple way to deeply clone an array or object
 const clone = x => JSON.parse(JSON.stringify(x))
 
-// enum to get next turn
+// An enum for the next turn in our game
 const NEXT_TURN = {
   O: 'X',
   X: 'O',
@@ -76,17 +91,28 @@ const reducer = (state, action) => {
   switch (action.type) {
     case 'CLICK': {
       const { x, y } = action.payload
+      // Since we need immutable updates, I often find the simplest thing to do
+      // is to clone the current state, and then use mutations on the clone to
+      // make updates for the next state
       const nextState = clone(state)
       const { grid, turn } = nextState
 
-      // if the cell already has a value, clicking on it should do nothing
-      if(grid[y][x]) {
+      // If the cell already has a value, clicking on it should do nothing
+      // Also, pay attention, because our rows are first, the `y` value is the
+      // first index, the `x` value second. This takes some getting used to.
+      if (grid[y][x]) {
         return state
       }
 
+      // If we're here in our program, we can assign this cell to the current
+      // `turn` value
       grid[y][x] = turn
 
+      // Now that we've used this turn, we need to set the next turn. It might
+      // be overkill, but I've used an object enum to do this.
       nextState.turn = NEXT_TURN[turn]
+
+      // We'll add checks for winning or drawing soon
 
       return nextState
     }
